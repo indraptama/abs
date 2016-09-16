13fd9108dc5f5bc2953f6c2083bb8ae9bca18a28
Indra Pratama Putra <indra@indras-Mac-Pro.local>
2016-09-16 08:29:44 +0700

    update

---
+++ app/js/img-fill.js
@@ -0,0 +1,17 @@
+(function() {
+  var test = document.querySelectorAll('main');
+
+  function addClassFull(imgContainer) {
+    var cssAttr = "background-size: cover; background-position: center; background-repeat: no-repeat";
+    for (var i = 0; i < imgContainer.length; i++) {
+      imgContainer[i].style.cssText += cssAttr;
+    }
+  };
+
+
+  function hideELem(targetContainer,srcClass) {
+    var
+  }
+
+addClassFull(test);
+}());
