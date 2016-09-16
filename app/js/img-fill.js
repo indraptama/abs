4e59c224f8f8955fde255722fd6dd86b20004567
Indra Pratama Putra <indra.symfonia@gmail.com>
2016-09-16 16:25:01 +0700

    update

--- app/js/img-fill.js
+++ app/js/img-fill.js
@@ -1,5 +1,6 @@
 (function() {
-  var test = document.querySelectorAll('main');
+  var test = document.querySelectorAll('.img-fill');
+  var imgContainer = document.querySelectorAll('.img-fill');
 
   function addClassFull(imgContainer) {
     var cssAttr = "background-size: cover; background-position: center; background-repeat: no-repeat";
@@ -9,9 +10,25 @@
   };
 
 
-  function hideELem(targetContainer,srcClass) {
-    var
+  function hideImgElem(imgContainer) {
+    for (var i = 0; i < imgContainer.length; i++) {
+      var imgSrc = imgContainer[i].querySelector('img');
+      imgSrc.style.display = 'none';
+    }
+  };
+
+  function applyImgBackground(imgContainer){
+    for (var i = 0; i < imgContainer.length; i++) {
+      var imgSrc = imgContainer[i].querySelector('img').src;
+      imgContainer[i].style.backgroundImage += "url('"+ imgSrc +"')";
+    }
   }
 
-addClassFull(test);
+  function imgFill(imgContainer) {
+    addClassFull(imgContainer)
+    hideImgElem(imgContainer)
+    applyImgBackground(imgContainer)
+  }
+
+  imgFill(test)
 }());
