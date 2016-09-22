9fa7ca1466998525526c1ad2f2d3428bc96643a2
Indra Pratama Putra <indra.symfonia@gmail.com>
2016-09-22 15:49:35 +0700

    update

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,28 @@
 var galThumbs = document.querySelectorAll('.galThumb');
+var Main = document.querySelector('Main');
+var mainDiv = Main.querySelectorAll('div');
 
 
+
+
+
+  window.addEventListener('scroll', function(){
+
+    for (var i = 0; i < mainDiv.length; i++) {
+      var theDiv = mainDiv[i];
+        Velocity(theDiv, { translateY: 10 }, 100);
+        Velocity(theDiv, { reverse: true }, 100);
+    }
+
+    console.log('this');
+  }, false)
 
 for (var i = 0; i < galThumbs.length; i++) {
   var galThumb = galThumbs[i];
   var galThumbCont = galThumb.querySelector('.galThumb-container');
 
-  galThumbCont.addEventListener('mouseover', function(){
-    Velocity(this.querySelector('.galThumb-img'), { translateX: 50 }, 500);
-    Velocity(this.querySelector('.galThumb-desc'), { translateX: 50 }, {delay: 150}, 500);
-  },false);
-
-  galThumbCont.addEventListener('mouseleave', function(){
-    Velocity(this.querySelector('.galThumb-img'), { translateX: 0 }, {delay: 150}, 500);
-    Velocity(this.querySelector('.galThumb-desc'), { translateX: 0 }, 500);
-  },false);
+  MouseHover(galThumbCont);
   //MouseHover(galThumbCont);
-
-  galThumbCont.addEventListener('focus', function(){
-    Velocity(this.querySelector('.galThumb-img'), { translateX: 50 }, 500);
-    Velocity(this.querySelector('.galThumb-desc'), { translateX: 50 }, {delay: 150}, 500);
-  },false);
-
-  galThumbCont.addEventListener('blur', function(){
-    Velocity(this.querySelector('.galThumb-img'), { translateX: 0 }, {delay: 150}, 500);
-    Velocity(this.querySelector('.galThumb-desc'), { translateX: 0 }, 500);
-  },false);
-
 };
 
 
@@ -34,11 +30,13 @@ for (var i = 0; i < galThumbs.length; i++) {
 
 
 function MouseHover(elem) {
-  elem.addEventListener('mouseover', function(){
-    Velocity(this, { translateX: 50 }, [ 500, 20 ]);
+  elem.addEventListener('mouseenter', function(){
+    Velocity(this.querySelector('.galThumb-img'), { translateX: 30 }, 500);
+    Velocity(this.querySelector('.galThumb-desc'), { translateX: 30 }, {delay: 150}, 500);
   },false);
 
   elem.addEventListener('mouseleave', function(){
-    Velocity(this, { translateX: 0 }, [ 500, 20 ]);
+    Velocity(this.querySelector('.galThumb-img'), { translateX: 0 }, {delay: 150}, 500);
+    Velocity(this.querySelector('.galThumb-desc'), { translateX: 0 }, 500);
   },false);
 }
